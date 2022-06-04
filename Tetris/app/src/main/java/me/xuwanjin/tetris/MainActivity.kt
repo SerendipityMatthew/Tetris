86c5fd460b5953dc63d24c41772ae9a40448aedb
xuwanjin <xuwj@mxchip.com>
2022-06-04 17:54:47 +0800

    add first commit
    Signed-off-by: xuwanjin <xuwj@mxchip.com>

---
+++ Tetris/app/src/main/java/me/xuwanjin/tetris/MainActivity.kt
@@ -0,0 +1,43 @@
+package me.xuwanjin.tetris
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import me.xuwanjin.tetris.ui.theme.TetrisTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TetrisTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TetrisTheme {
+        Greeting("Android")
+    }
+}
